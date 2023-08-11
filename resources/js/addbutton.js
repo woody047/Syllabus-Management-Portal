5f50cce6978ef384b2fe108d2475517f9f72aaf4
woody047 <tzw97226@gmail.com>
2023-08-11 18:31:12 +0800

    complete the course syllabus creation and update, audit log recorded the remaining record, but the audit log search engine not workable

--- resources/js/addbutton.js
+++ resources/js/addbutton.js
@@ -5,18 +5,18 @@ $(document).ready(function () {
         rowCount++;
         // Create a new row with input fields
         let row = `
-            <tr>
-                <td><textarea name="courseOutline[]" rows="5" cols="50" style="width:500px; height:200px;"></textarea></td>           
-                <td><input type="text" name="CO[]" style="width:100px"></input></td>
-                <td><input type="text" name="L[]" style="width:20px"></input></td>
-                <td><input type="text" name="T[]" style="width:20px"></input></td>
-                <td><input type="text" name="P[]" style="width:20px"></input></td>
-                <td><input type="text" name="O[]" style="width:20px"></input></td>
-                <td><input type="text" name="GuidedLearning[]" style="width:20px"></input></td>
-                <td><input type="text" name="IndependentLearning[]" style="width:20px"></input></td>
-                <td><input type="text" name="TotalSLT[]" style="width:20px"></input></td>
-            </tr>
-        `;
+        <tr>
+            <td><textarea type="text" name="courseOutline[]" rows="5" cols="50" style="width:500px; height:200px;" required></textarea></td>           
+            <td><input type="text" name="CO[]" style="width:100px" required></input></td>
+            <td><input type="text" name="L[]" style="width:20px" required></input></td>
+            <td><input type="text" name="T[]" style="width:20px" required></input></td>
+            <td><input type="text" name="P[]" style="width:20px" required></input></td>
+            <td><input type="text" name="O[]" style="width:20px" required></input></td>
+            <td><input type="text" name="GuidedLearning[]" style="width:20px" required></input></td>
+            <td><input type="text" name="IndependentLearning[]" style="width:20px" required></input></td>
+            <td><input type="text" name="TotalSLT[]" style="width:20px" required></input></td>
+        </tr>
+`;
 
         // Append the new row to the table body
         $('#table-body').append(row);
